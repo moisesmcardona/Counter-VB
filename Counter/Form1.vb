490847d945462600e873547a8b84791c74301d5b
Moises Cardona <moises@moises-studios.com>
2018-01-19 11:17:53 -0500

    Add project files.

---
+++ Counter/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Label1.Text = Label1.Text + 1
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Label1.Text = Label1.Text - 1
+    End Sub
+End Class
